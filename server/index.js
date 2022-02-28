e9f36b4850eb3ad7678fc01c16774f1960ee69e2
Jinypak <79570234+Jinypak@users.noreply.github.com>
2022-02-28 11:47:24 +0900

    폼 전송 및 db 세팅 테스트

--- server/index.js
+++ server/index.js
@@ -1,11 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const mysql = require('mysql');
+const cors = require("cors");
+const mysql = require("mysql");
 
 app.use(cors());
-// db = mysql.createConnection({
+app.use(express.json());
 
-// })
+const db = mysql.createConnection({
+  user: "yg_admin",
+  host: "ls-64a316e1863dbd10d24178a071ec8fa22bec593f.ciilmyupcbcw.ap-northeast-2.rds.amazonaws.com",
+  password: ".o);VrW&Dl{0{0^;78-sF`tkEVY]nOo2",
+  database: "Customers",
+});
 
-app.listen(4000, ()=>{console.log("back running")})
+app.post("/", (req, res) => {
+  const name = req.body.name;
+  const nation = req.body.nation;
+  const email = req.body.email;
+  const phone = req.body.phone;
+  const message = req.body.message;
+  const sqlPost =
+    "INSERT INTO customer (name, nation, email, phone, message) VALUES (?, ?, ?, ?, ?)";
+  db.query(sqlPost, [name, nation, email, phone, message], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      () => res.send(result);
+    }
+  });
+});
+
+app.listen(4000, () => {
+  console.log("back running");
+});
