81444c4ce783f4db8d8510f36be231ccb880007a
Park Jinyong <79570234+Jinypak@users.noreply.github.com>
2022-03-04 20:26:21 +0900

    aa

--- server/index.js
+++ server/index.js
@@ -54,7 +54,7 @@ app.listen(4000, () => {
 //   var today = new Date();
 //   var year = today.getFullYear();
 //   var month = ("0" + (today.getMonth() + 1)).slice(-2);
-//   var day = ("0" + (today.getDate())).slice(-2);
+//   var day = ("0" + (today.getDate()-1)).slice(-2);
 //   var dateString = year + "-" + month + "-" + day;
 
 
