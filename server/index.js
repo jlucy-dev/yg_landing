4329f058f6e917eec64a352cf98b8831d865daed
Park Jinyong <79570234+Jinypak@users.noreply.github.com>
2022-03-07 16:18:31 +0900

    check update

--- server/index.js
+++ server/index.js
@@ -6,8 +6,15 @@ const requstip = require("request-ip");
 const cors = require("cors");
 const mysql = require("mysql");
 
-app.use(cors());
+let corsOptions = {
+  origin : 'http://localhost:5500',
+  credentials: true,
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
+
+
 
 const db = mysql.createConnection({
   user: "yg_admin",
@@ -37,11 +44,42 @@ app.post("/", (req, res) => {
   });
 });
 
+
 // 아이피 주소 받아오기 IPv6
-app.get("/", (req, res) => {
-  res.send("ip check");
-  console.log("client IP" + requstip.getClientIp(req));
-});
+// app.get("/cors", (req, res) => {
+//   const handleCountries = () => {
+//     var myHeaders = new Headers();
+//     myHeaders.append(
+//       "Access-Control-Allow-Headers", "Content-Type",
+//       "Access-Control-Allow-Origin", "*",
+//       "Access-Control-Allow-Methods", "OPTIONS,POST,GET");
+
+//     var requestOptions = {
+//       method: "GET",
+//       headers: myHeaders,
+//       redirect: "follow",
+//     };
+
+//     fetch(
+//         `http://whois.kisa.or.kr/openapi/ipascc.jsp?query=175.196.54.203&key=2022030312202739331643&answer=json`,
+//         // `http://whois.kisa.or.kr/openapi/ipascc.jsp?query=${locationCheck}&key=2022030312202739331643&answer=json`,
+//         requestOptions
+//       )
+//       .then((response) => response.json())
+//       .then((result) => {
+//         console.log(result)
+//         // let nationCode = result.whois.countryCode.toLowerCase();
+//         // console.log(nationCode);
+//         // if (nationCode) {
+//         //   window.location.href = `http://3.38.166.125/pages/index_${nationCode}.html`;
+//         // } else {
+//         //   window.location.href = `http://3.38.166.125/pages/index_kr.html`;
+//         // }
+//       })
+//       .catch((error) => console.log("error"));
+//   };
+//   res.send(handleCountries);
+// });
 
 app.get("/check", (req, res) => {
   res.send("서버 연결 확인");
@@ -82,61 +120,61 @@ const mailOptions = {
   }]
 }
 
-setInterval(() => {
-  function check() {
-    let sqlDate = `SELECT name, nation, contact, message, date FROM customer WHERE date LIKE "2022-${month}-${day}%"`;
-    db.query(sqlDate, (err, rows) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(rows);
-        let result = JSON.parse(JSON.stringify(rows));
-        addExcel(result)
-      }
-    });
-  }
-  check();
+// setInterval(() => {
+//   function check() {
+//     let sqlDate = `SELECT name, nation, contact, message, date FROM customer WHERE date LIKE "2022-${month}-${day}%"`;
+//     db.query(sqlDate, (err, rows) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         console.log(rows);
+//         let result = JSON.parse(JSON.stringify(rows));
+//         addExcel(result)
+//       }
+//     });
+//   }
+//   check();
 
-  function addExcel(event) {
-    console.log(`Sending Day info ${month}-${day}`)
-    const workbook = new Exceljs.Workbook();
-    workbook.creator = "jlucy";
-    const worksheet = workbook.addWorksheet("유저 리스트");
-    worksheet.columns = [{
-        header: '이름',
-        key: 'name'
-      },
-      {
-        header: '국가',
-        key: 'nation'
-      },
-      {
-        header: '연락처',
-        key: 'contact'
-      },
-      {
-        header: '문의내용',
-        key: 'message'
-      },
-      {
-        header: '날짜',
-        key: 'date'
-      },
-    ]
-    const rawData = event;
-    console.log(rawData)
-    rawData.map((data, index) => {
-      worksheet.addRow(data)
-    })
-    workbook.xlsx.writeFile(`./user_info_${day}.xlsx`);
-    transporter.sendMail(mailOptions, (err, info) => {
-      if (err) {
-        console.log("error message" + err)
-      } else {
-        console.log("message send!" + info.response)
-      }
-      transporter.close();
-    })
-  }
+//   function addExcel(event) {
+//     console.log(`Sending Day info ${month}-${day}`)
+//     const workbook = new Exceljs.Workbook();
+//     workbook.creator = "jlucy";
+//     const worksheet = workbook.addWorksheet("유저 리스트");
+//     worksheet.columns = [{
+//         header: '이름',
+//         key: 'name'
+//       },
+//       {
+//         header: '국가',
+//         key: 'nation'
+//       },
+//       {
+//         header: '연락처',
+//         key: 'contact'
+//       },
+//       {
+//         header: '문의내용',
+//         key: 'message'
+//       },
+//       {
+//         header: '날짜',
+//         key: 'date'
+//       },
+//     ]
+//     const rawData = event;
+//     console.log(rawData)
+//     rawData.map((data, index) => {
+//       worksheet.addRow(data)
+//     })
+//     workbook.xlsx.writeFile(`./user_info_${day}.xlsx`);
+//     transporter.sendMail(mailOptions, (err, info) => {
+//       if (err) {
+//         console.log("error message" + err)
+//       } else {
+//         console.log("message send!" + info.response)
+//       }
+//       transporter.close();
+//     })
+//   }
 
-}, 43200000);
+// }, 43200000);
